be6e6db6da672d92d57b9ed09bc8d4b1bd19c961
Yuchan Han <serltretu24@gmail.com>
2023-10-07 16:49:03 +0900

    feat: shortcut 생성 스크립트

---
+++ steambox/shortcut.ps1
@@ -0,0 +1,73 @@
+$dir = 'C:\Program Files (x86)\Steam\steamapps'
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+
+$shell = New-Object -ComObject WScript.Shell
+
+$appManifests = Get-ChildItem -Path $dir -Filter 'appmanifest_*.acf' -File
+
+function Remove-StringFromLine {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$FilePath,
+        
+        [Parameter(Mandatory=$true)]
+        [string]$TargetString
+    )
+
+    $fileContent = Get-Content $FilePath
+
+    foreach ($line in $fileContent) {
+        if ($line.Contains($TargetString)) {
+            $cleanedString = $line -replace $TargetString, ""
+            $cleanedString = [regex]::Replace($cleanedString, '(?<=^([^"]*"[^"]*")*[^"]*)\s', '')
+            $cleanedString = $cleanedString -replace '"', ""
+
+            return $cleanedString
+        }
+    }
+}
+
+function FindIconFile($folder) {
+    $iconFiles = Get-ChildItem -Path $folder -Filter "*.ico" -File
+
+    if ($iconFiles.Count -eq 0) {
+        $iconFiles = Get-ChildItem -Path $folder -Filter "*.exe" -File
+    }
+
+    if ($iconFiles.Count -gt 0) {
+        return $iconFiles[0].FullName
+    }
+
+    $subFolders = Get-ChildItem -Path $folder | Where-Object { $_.PSIsContainer }
+
+    foreach ($subFolder in $subFolders) {
+        $iconFilePath = FindIconFile($subFolder.FullName)
+
+        if ($null -ne $iconFilePath) {
+            return $iconFilePath
+        }
+    }
+
+    return $null
+}
+
+
+foreach ($file in $appManifests) {
+    $gameId = Remove-StringFromLine -FilePath "$($file.FullName)" -TargetString 'appid'
+    $gameName = Remove-StringFromLine -FilePath "$($file.FullName)" -TargetString 'name'
+    $gameFolderName = Remove-StringFromLine -FilePath "$($file.FullName)" -TargetString 'installdir'
+    $gameIconLocation = FindIconFile -folder "$($dir)\common\$($gameFolderName)"
+
+    Write-Host "Game ID: $gameId"
+    Write-Host "Game Name: $gameName"
+
+    $shortcut = $shell.CreateShortcut("$desktopPath\$gameName.lnk")
+    $shortcut.TargetPath = "`"C:\Program Files (x86)\Steam\Steam.exe`" -applaunch $gameId"
+    $shortcut.IconLocation = "$($gameIconLocation), 0"
+    $shortcut.Save()
+
+    Write-Output "---"
+
+
+
+}
